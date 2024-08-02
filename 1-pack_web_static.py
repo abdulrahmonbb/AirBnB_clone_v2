ffbaf199b49e13ceb72eff4db3db8e9de42030fb
rahmoncodes <abdulrahmonbb@gmail.com>
2024-08-02 18:59:51 +0100

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script generates a tgz archive from the contents of the web_static folder
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+def do_pack():
+    """
+    generates a tgz archive
+    """
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = f'versions/web_static_{date}.tgz'
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
