1aee5d4043894ec7ca4ebf18176c101d9835df56
rahmoncodes <abdulrahmonbb@gmail.com>
2024-08-08 09:45:18 +0100

    2-do_deploy_web_static.py v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,51 +1,51 @@
 #!/usr/bin/python3
 """
-This script distributes the archive on the web servers.
+This Fabric script (based on the file 1-pack_web_static.py) 
+distributes an archive to the web servers
+using the function do_deploy
 """
 from fabric.api import *
 from os.path import exists
 
-# Web servers IP addresses
 env.hosts = ['100.25.171.73', '54.210.174.198']
-
 
 def do_deploy(archive_path):
     """
-    Uploads and Uncompresses the web static archive on the
-    web servers.
+    Distributes an archive to the web servers.
     """
+    # Check if file exists in path, return False if not
     if exists(archive_path) is False:
-        # returns false if file at archive path does not exist.
         return False
 
-    # remove absolute path before the filename
+    # Get the file name
     filename = archive_path.split('/')[-1]
 
-    # folder to uncompress archive
-    no_extension = '/data/web_static/releases/' + '{}'.format(filename.split('.')[0])
-
+    # Remove extension from filename and build folder name
+    no_ext = '/data/web_static/releases/' + filename.split('.')[0]
+    
+    # Create file path to tmp
     tmp = '/tmp/' + filename
 
     try:
-        # upload archive to /tmp/ directory of the web server
+        # Upload the archive to /tmp/ directory of the web server
         put(archive_path, '/tmp/')
 
-        # create the folder to uncompress the archive
-        run('mkdir -p {}'.format(no_extension))
-        
-        # uncompress the archive into the folder
-        run('tar -xzvf {} -C {}'.format(tmp, no_extension))
-        
-        # deleting the archive from the web server
+        # Create the folder that will contain uncompressed files
+        run('mkdir -p {}'.format(no_ext))
+
+        # Uncompress the archive to the created folder
+        run('tar -xzvf {} -C {}'.format(tmp, no_ext))
+
+        # Delete the archive from the web server
         run('rm {}'.format(tmp))
-        run('mv {}/web_static/* {}/'.format(no_extension, no_extension))
-        run('rm -rf {}/web_static/'.format(no_extension))
-        
-        # delete the symbolic link /data/web_static/current from the web server
+        run('mv {}/web_static/* {}/'.format(no_ext, no_ext))
+        run('rm -rf {}/web_static'.format(no_ext))
+
+        # Delete the symbolic link /data/web_static/current from the web server
         run('rm -rf /data/web_static/current')
 
-        # create a new the symbolic link /data/web_static/current on the web server
-        run('ln -s {}/ /data/web_static/current'.format(no_extension))
+        # Create a new symbolic link on the web server linked to the new version of the code
+        run('ln -s {} /data/web_static/current'.format(no_ext))
         return True
     except Exception as e:
         return False
