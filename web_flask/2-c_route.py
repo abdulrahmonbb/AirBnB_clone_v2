effd7b1e195227e25e1b5dd52ca3d05b60058952
rahmoncodes <abdulrahmonbb@gmail.com>
2024-08-17 18:45:06 +0100

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application.
+starts a Flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
@@ -21,11 +20,9 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """displays c followed by the value of <text>"""
-    text = text.replace('_', ' ')
-    return f'c {text}'
-
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
